5f743794c4c8fdbe249114ee954a78b7a4da69bc
kisiohlova <kisioglovan@gmail.com>
2023-10-13 21:47:55 +0300

    Closed #6 test: add model tests

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   validates :title, :author, :isbn, presence: true
-  validates :isbn, uniqueness: true, length: { minimum: 10, maximum: 13}
+  validates :isbn, uniqueness: true
   validates :description, length: { maximum: 2000 }
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_book) { create(:book) }
+  let(:invalid_book) { build(:book, title: nil) }
+
+  describe 'Validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:author) }
+    it { should validate_presence_of(:isbn) }
+    it { should validate_uniqueness_of(:isbn) }
+    it { should validate_length_of(:description).is_at_most(2000) }
+  end
+
+  it "is valid with valid attributes" do
+    expect(valid_book).to be_valid
+  end
+
+  it "is not valid without a title" do
+    expect(invalid_book).not_to be_valid
+  end
 end
