f196234fce5472aca62d4d16ab420763ba1ff0f4
kisiohlova <kisioglovan@gmail.com>
2023-10-13 18:36:01 +0300

    Closed #3 feat: add model validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  validates :title, :author, :isbn, presence: true
+  validates :isbn, uniqueness: true, length: { minimum: 10, maximum: 13}
+  validates :description, length: { maximum: 2000 }
 end
