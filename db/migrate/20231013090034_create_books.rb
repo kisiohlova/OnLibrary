80a8c0fc9d3e47ffaff9123c182cb7c63f7a9059
kisiohlova <kisioglovan@gmail.com>
2023-10-30 18:19:35 +0200

    refactor: rollback migrations

--- db/migrate/20231013090034_create_books.rb
+++ db/migrate/20231013090034_create_books.rb
@@ -1,9 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-      t.string :title
-      t.string :author
-      t.string :isbn
+      t.string :title, null: false
+      t.string :author, null: false
+      t.string :isbn, null: false
       t.text :description
 
       t.timestamps
--- db/migrate/20231018154410_change_columns_not_null.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnsNotNull < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :books, :description, true
-    change_column_null :books, :isbn, false
-  end
-end
