0e8e23238e269757e1a0ac1be804ff583c45f4ea
kisiohlova <kisioglovan@gmail.com>
2023-11-12 13:28:08 +0200

    change book isbn validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
   validates :title, length: { maximum: 150 }
   validates :description, length: { maximum: 1000 }
   validates :isbn, uniqueness: { case_sensitive: false }
-  validates :isbn, format: { with: /\A\d[\d-]*\d\z/, message: "only allows numbers and dashes" }
+  validates :isbn, format: { with: /\A(?:\d[\-]?){9}[\dXx]$|^(?:\d[\-]?){13}\z/, message: "only allows 10 or 13 digits, and optional hyphens" }
 
   scope :ordered, -> { order(:title) }
 end
