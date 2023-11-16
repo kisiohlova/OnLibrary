df21cbcbe44029ef7cc01129022b18566f63d691
kisiohlova <kisioglovan@gmail.com>
2023-11-16 16:13:33 +0200

    change book factory

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -6,15 +6,11 @@ FactoryBot.define do
     description { Faker::Lorem.paragraph }
 
     trait :with_cover do
-      after(:build) do |book|
-        book.cover.attach(io: File.open(Rails.root.join("spec", "fixtures", "files", "example_cover.jpg")), filename: "example_cover.jpg", content_type: "image/jpeg")
-      end
+      cover { Rack::Test::UploadedFile.new("spec/fixtures/files/example_cover.jpg", "image/jpeg") }
     end
 
     trait :with_content do
-      after(:build) do |book|
-        book.content.attach(io: File.open(Rails.root.join("spec", "fixtures", "files", "example_content.pdf")), filename: "example_content.pdf", content_type: "application/pdf")
-      end
+      content { Rack::Test::UploadedFile.new("spec/fixtures/files/example_content.pdf", "application/pdf") }
     end
   end
 end
